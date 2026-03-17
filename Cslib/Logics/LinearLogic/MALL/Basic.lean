0a32bfea17d0a69478622ecd7c9a679514c32b34
Matteo Acclavio <macclavio@gmail.com>
2026-03-17 15:02:10 +0100

    incagliato

--- Cslib/Logics/LinearLogic/MALL/Basic.lean
+++ Cslib/Logics/LinearLogic/MALL/Basic.lean
@@ -12,55 +12,54 @@ public import Cslib.Foundations.Logic.InferenceSystem
 public import Cslib.Foundations.Logic.LogicalEquivalence
 public import Mathlib.Data.Multiset.Fold
 public import Mathlib.Data.Finset.Insert
+public import Mathlib.Data.Finset.Card
 public import Mathlib.Data.Finset.Union
 public import Cslib.Foundations.Data.FinFun
+public import Mathlib.Data.List.MinMax
 
 @[expose] public section
 
-/-! # Multiplicative Additive Linear Logic (MALL)
-
+/-!
+# MALL := (unit-free) Multiplicative Additive Linear Logic
 ## References
-
-* [J.-Y. Girard, *Linear Logic: its syntax and semantics*][Girard1995]
+* [J.-Y. Girard, *Linear logic*][Girard1987]
 
 -/
 
 namespace Cslib.Logic
 
-universe u
 
+universe u
 variable {Atom : Type u}
 
 namespace MALL
 
+
+
 /-- Propositions. -/
 inductive Proposition (Atom : Type u) : Type u where
+  /-- Literals -/
   | atom (x : Atom)
   | atomDual (x : Atom)
-  -- | one
-  -- | zero
-  -- | top
-  -- | bot
-  /-- The multiplicative conjunction connective. -/
+  /-- Multiplicative conjunction. -/
   | tensor (a b : Proposition Atom)
-  /-- The multiplicative disjunction connective. -/
+  /-- Multiplicative disjunction. -/
   | parr (a b : Proposition Atom)
-  /-- The additive disjunction connective. -/
-  | oplus (a b : Proposition Atom)
-  /-- The additive conjunction connective. -/
+  /-- Additive  conjunction. -/
   | with (a b : Proposition Atom)
+  /-- Additive  disjunction. -/
+  | oplus (a b : Proposition Atom)
 deriving DecidableEq, BEq
 
--- instance : Zero (Proposition Atom) := ⟨.zero⟩
--- instance : One (Proposition Atom) := ⟨.one⟩
-
--- instance : Top (Proposition Atom) := ⟨.top⟩
--- instance : Bot (Proposition Atom) := ⟨.bot⟩
-
 @[inherit_doc] scoped infix:35 " ⊗ " => Proposition.tensor
-@[inherit_doc] scoped infix:35 " ⊕ " => Proposition.oplus
 @[inherit_doc] scoped infix:30 " ⅋ " => Proposition.parr
 @[inherit_doc] scoped infix:30 " & " => Proposition.with
+@[inherit_doc] scoped infix:35 " ⊕ " => Proposition.oplus
+
+/-- A sequent in MALL is a multiset of propositions. -/
+abbrev Sequent Atom := Multiset (Proposition Atom)
+
+
 
 /-- Propositional duality. -/
 @[scoped grind =]
@@ -75,19 +74,16 @@ def Proposition.dual : Proposition Atom → Proposition Atom
 @[inherit_doc] scoped postfix:max "⫠" => Proposition.dual
 
 
-/-- A sequent in MALL is a multiset of propositions. -/
-abbrev Sequent Atom := Multiset (Proposition Atom)
-
 open Proposition in
-/-- A proof in the sequent calculus for classical linear logic. -/
-inductive Proof : Sequent Atom → Type u where
-  | ax : Proof {a, a⫠}
-  | cut : Proof (a ::ₘ Γ) → Proof (a⫠ ::ₘ Δ) → Proof (Γ + Δ)
-  | parr : Proof (a ::ₘ b ::ₘ Γ) → Proof ((a ⅋ b) ::ₘ Γ)
-  | tensor : Proof (a ::ₘ Γ) → Proof (b ::ₘ Δ) → Proof ((a ⊗ b) ::ₘ (Γ + Δ))
-  | oplus₁ : Proof (a ::ₘ Γ) → Proof ((a ⊕ b) ::ₘ Γ)
-  | oplus₂ : Proof (b ::ₘ Γ) → Proof ((a ⊕ b) ::ₘ Γ)
-  | with : Proof (a ::ₘ Γ) → Proof (b ::ₘ Γ) → Proof ((a & b) ::ₘ Γ)
+/-- A derivation in the sequent calculus for MALL. -/
+inductive Derivation : Sequent Atom → Type u where
+  | ax : Derivation {a, a⫠}
+  | cut : Derivation (a ::ₘ Γ) → Derivation (a⫠ ::ₘ Δ) → Derivation (Γ + Δ)
+  | parr : Derivation (a ::ₘ b ::ₘ Γ) → Derivation ((a ⅋ b) ::ₘ Γ)
+  | tensor : Derivation (a ::ₘ Γ) → Derivation (b ::ₘ Δ) → Derivation ((a ⊗ b) ::ₘ (Γ + Δ))
+  | oplus₁ : Derivation (a ::ₘ Γ) → Derivation ((a ⊕ b) ::ₘ Γ)
+  | oplus₂ : Derivation (b ::ₘ Γ) → Derivation ((a ⊕ b) ::ₘ Γ)
+  | with : Derivation (a ::ₘ Γ) → Derivation (b ::ₘ Γ) → Derivation ((a & b) ::ₘ Γ)
 
 namespace ProofNet
 
@@ -96,203 +92,346 @@ namespace ProofNet
 
 
 ----------------------------
--- Trees with unique identifier for each node, and well-formedness condition
-----------------------------
-
-inductive BinTree (α : Type*) where
-  | leaf (a : α)
-  | node (a : α) (t₁ : BinTree α) (t₂ : BinTree α)
-
-
-def BinTree.names [DecidableEq α] (t : BinTree α) : Finset α :=
-  match t with
-  | leaf a        => {a}
-  | node a t₁ t₂  => {a} ∪ t₁.names ∪ t₂.names
-
-def BinTree.wellFormed [DecidableEq α] (t : BinTree α) : Prop :=
-  match t with
-  | leaf _ => True
-  | node a t₁ t₂ => Disjoint t₁.names t₂.names ∧ a ∉ t₁.names ∪ t₂.names
-
-
-----------------------------
--- Formula Trees
-----------------------------
-inductive PropositionBinTree.Value (Atom : Type u) where
-  | atom (a : Atom)
-  | atomDual (a : Atom)
-  | tensor
-  | parr
-  | oplus
-  | with
-
-def PropositionBinTree (Atom Name : Type*) [DecidableEq Name] :=
-  Σ (t : BinTree Name), ({n // n ∈ t.names} → PropositionBinTree.Value Atom)
-
-
-
--- ----------------------------
--- -- HERE COULD BE REDUNDANT
--- ----------------------------
--- -- M: is if ok that we rename the seuents?
--- abbrev Sequent (Atom Name : Type*) [DecidableEq Name] := Multiset (PropositionBinTree Atom Name)
-
-
--- -- M this may be useless now we have BinForest.wellFormed
--- def Sequent.wellFormed [DecidableEq Name] (Γ : Sequent Atom Name) :=
---   Γ.map (BinTree.names ·.1)
---   |> Multiset.fold (Disjoint · ·)
---   -- |> Multiset.fold (· ∪ ·) ∅
--- ----------------------------
--- ----------------------------
-
-
-
-----------------------------
--- TO REVISE
-----------------------------
-
-----------------------------
 --  Trees
+-- MA we should rename the current trees in the cslib to BinTree,
+-- and replace them with these ones
 ----------------------------
+
+/-- Define Tree data structure, nodes can have any number of children -/
 inductive Tree (α : Type*) where
   | leaf (a : α)
-  | node : α → List (Tree α) → Tree α
+  | node (a : α) (children : List (Tree α))
 deriving BEq
 
+
+-- instance {α : Type*} [DecidableEq α] : DecidableEq (Tree α)
+-- | Tree.leaf a, Tree.leaf b =>
+--   if h : a = b then isTrue (by rw [h]) else isFalse (by intro H; injection H; contradiction)
+-- | Tree.node a childrenA, Tree.node b childrenB =>
+--   if h1 : a = b then
+--     if h2 : childrenA.length = childrenB.length then
+--       let decs := List.zipWith (fun t1 t2 => DecidableEq.decEq t1 t2) childrenA childrenB
+--       if decs.all (fun d => match d with | isTrue _ => true | _ => false) then
+--         isTrue (by
+--           intro H
+--           injection H with _ hChildren
+--           have : childrenA = childrenB := hChildren
+--           subst this
+--           rfl)
+--       else
+--         isFalse (by
+--           intro H
+--           injection H with _ hChildren
+--           have : childrenA = childrenB := hChildren
+--           subst this
+--           have : ¬List.zipWith (fun t1 t2 => DecidableEq.decEq t1 t2) childrenA childrenB |>.all (fun d => match d with | isTrue _ => true | _ => false) := by assumption
+--           contradiction)
+--     else
+--       isFalse (by intro H; injection H with _ hChildren; rw [h1] at hChildren; contradiction)
+--   else isFalse (by intro H; injection H with h1 _; contradiction)
+-- | _, _ => isFalse (by intro H; cases H)
+
+
+/-- Names of the nodes in the tree -/
 def Tree.names [DecidableEq α] (t : Tree α) : Finset α :=
   match t with
-  | leaf a        => {a}
+  | leaf a          => {a}
   | node a children => {a} ∪ children.foldl (fun acc t => acc ∪ t.names) ∅
 
-def isNodeOf [DecidableEq α] (n : α) : (t: Tree α) → Bool :=
-  fun t => n ∈ t.names
+/-- check if a name is in a tree -/
+def isNodeOf [DecidableEq α] (n : α) (t : Tree α) : Prop :=
+  n ∈ t.names
+deriving Decidable
 
-def isLeafOf [DecidableEq α] (n : α) : Tree α → Bool
-  | Tree.leaf a => n == a
-  | Tree.node a children =>
-      (n != a) &&
-      children.foldr (fun t acc => isLeafOf n t || acc) false
+notation:50 n " ∈ " t => isNodeOf n t
 
+/-- Names of the nodes in the tree which are leaves (i.e., no children) -/
 def Tree.leaves [DecidableEq α] (t : Tree α) : Finset α :=
-  t.names.filter (fun n => isLeafOf n t)
+  match t with
+  | leaf a          => {a}
+  | node _ children => children.foldl (fun acc t => acc ∪ t.leaves) ∅
 
+/-- Names of the nodes in the tree which are internal (i.e., have children) -/
 def Tree.internalNodes [DecidableEq α] (t : Tree α) : Finset α :=
-  t.names.filter (fun n => ¬ isLeafOf n t)
+  t.names.filter (fun n => !(n ∈ t.leaves))
 
-def Tree.root : Tree α → α
-  | Tree.leaf a => a
-  | Tree.node a _ => a
+-- returns the root of a tree
+def Tree.root (t : Tree α) : Finset α :=
+  match t with
+  | Tree.leaf a => {a}
+  | Tree.node a _ => {a}
 
-def IsSubtreeOf [BEq α] (t₁ t₂ : Tree α) : Bool :=
-  t₁ == t₂ ||
-  match t₂ with
-  | Tree.leaf _ => false
-  | Tree.node _ children =>
-    children.foldr (fun t acc => IsSubtreeOf t₁ t || acc) false
 
+/-- Height of a tree, i.e., the length of the longest path from the root -/
+def Tree.height : Tree α → ℕ
+| Tree.leaf _ => 0
+| Tree.node _ children =>
+  1 + List.foldl max 0 (children.map Tree.height)
+
+
+/-- Height of a tree, i.e., the length of the longest path from the root -/
+def Tree.size : Tree α → ℕ
+| Tree.leaf _ => 1
+| Tree.node _ children =>
+  1 + List.sum (children.map Tree.size)
+
+
+lemma max_le_sum {a b c d : ℕ} (h1 : a ≤ c) (h2 : b ≤ d) : max a b ≤ c + d := by
+  cases  (Nat.le_total a b)
+  case inl h =>
+    omega
+  case inr h =>
+    omega
+
+
+
+theorem Tree.boundHeight {t : Tree α} : t.height ≤ t.size := by
+  match t with
+  | leaf a =>
+    simp [Tree.height, Tree.size]
+  | node a children =>
+    simp [Tree.height, Tree.size]
+    induction children with
+    | nil => simp
+    | cons head tail ih =>
+      simp [List.map]
+      rw [List.foldl_max]
+      apply max_max_le_sum_sum
+      · exact Tree.boundHeight (t := head)
+      · rw [List.foldl_max] at ih
+        rw [List.foldl_max]
+
+--- MI SENTO STUPIDO
+
+
+
+
+
+
+/-- Check that each name appears at most once in a tree -/
 def Tree.wellFormed [DecidableEq α] (t : Tree α) : Prop :=
   match t with
-  | leaf _ => True
-  | node a children =>
-    Disjoint (children.foldl (fun acc t => acc ∪ t.names) ∅) {a}
+  | Tree.leaf _ => True
+  | Tree.node _ children =>
+    t.names.card = t.size
     ∧
-    children.all (
-      fun t =>
-      Disjoint t.names (children.foldl (fun acc t => acc ∪ t.names) ∅ \ t.names)
-    )
+    children ≠ []
+    ∧
+    (∀ t ∈ children, Tree.wellFormed t)
+
+
+
+
+
+
 
 ----------------------------
 -- Forests
 ----------------------------
 
+-- forest is a multiset of trees
 def Forest (α : Type*) := Multiset (Tree α)
 
+-- well formed forest is a forest where all nodes have disjoint names
 def Forest.wellFormed [DecidableEq α] (f : Forest α) : Prop :=
   let trees := f.toList
   (∀ t ∈ trees, Tree.wellFormed t) ∧
   trees.Pairwise (fun t₁ t₂ => Disjoint t₁.names t₂.names)
 
-def subForest [DecidableEq α] (f₁ f₂ : Forest α) : Prop :=
-  ∀ t₁ ∈ f₁.toList, ∃ t₂ ∈ f₂.toList, IsSubtreeOf t₁ t₂
+-- returns the set of names in a forest
+noncomputable def Forest.names [DecidableEq α] (f : Forest α) : Finset α :=
+  f.toList.foldl (fun acc t => acc ∪ t.names) ∅
 
-noncomputable def Forest.roots [DecidableEq α] : (f : Forest α) → Multiset α
-  | f => f.toList.foldl (fun acc t => acc + {t.root}) ∅
+-- returns the set of roots of the trees in a forest
+noncomputable def Forest.roots [DecidableEq α] (f : Forest α) : Finset α :=
+  f.toList.foldl (fun acc t => acc ∪ t.root) ∅
+
+-- check if a name is in the forest, and defines the notation for it
+def isTreeInForest [DecidableEq α] (t : Tree α) (f : Forest α) : Prop :=
+  t ∈ f.toList
+
+
+
+notation:50 t " ∈ₜ " f => isTreeInForest t f
+
 
 ----------------------------
 -- labels we need for our trees
 ----------------------------
-inductive PropositionTree.Value (Atom : Type u) where
-  | atom (a : Atom)
-  | atomDual (a : Atom)
+inductive FormulaTree.Value (Label : Type u) where
+  | atom (a : Label)
+  | atomDual (a : Label)
   | tensor
   | parr
   | oplus
   | with
 
--- If we can separate them, could be nice
-inductive PropositionTree.Atoms (Atom : Type u) where
-  | atom (a : Atom)
-  | atomDual (a : Atom)
 
-inductive PropositionTree.Operators (Atom : Type u) where
-  | tensor
-  | parr
-  | oplus
-  | with
+def isLiteral [DecidableEq Label] (v : FormulaTree.Value Label) : Prop :=
+  match v with
+  | .atom _ => True
+  | .atomDual _ => True
+  | _ => False
 
-inductive CoTree.Label (Atom : Type u) where
+inductive CoTree.Label (Label : Type u) where
   | conf
   | conc
 
 
 ----------------------------
--- Trees for formulas and Forests for sequents
+-- Formula-trees
 ----------------------------
 
-def PropositionTree (Atom Name : Type*) [DecidableEq Name] :=
-  Σ (t : Tree Name), ({n // n ∈ t.names} → PropositionTree.Value Atom)
+-- A tree of nodes labeled by formulas
+def FormulaTree (Atom Nat : Type*) [DecidableEq Nat] :=
+  Σ (t : Tree Nat), ({ n : Nat // n ∈ t } → FormulaTree.Value Atom)
 
-def PropositionForest (Atom Name : Type*) [DecidableEq Name] :=
-  Σ (f : Forest Name), ∀ t, t ∈ f.toList → PropositionTree.Value Atom
+-- Check that a formula tree is well-formed
+def FormulaTree.wellFormed [DecidableEq Nat] (t : FormulaTree Atom Nat ) : Prop :=
+  let ⟨tree, labeling⟩ := t
+  Tree.wellFormed tree ∧
+  ∀ (n : Nat) (hn : n ∈ tree),
+    match labeling ⟨n, hn⟩ with
+    | .atom _     => n ∈ tree.leaves
+    | .atomDual _ => n ∈ tree.leaves
+    | .tensor     => n ∉ tree.leaves
+    | .parr       => n ∉ tree.leaves
+    | .oplus      => n ∉ tree.leaves
+    | .with       => n ∉ tree.leaves
 
-noncomputable def PropositionForest.names [DecidableEq Name] (f : PropositionForest Atom Name) :
-  Finset Name :=f.1.toList.foldl (fun acc t => acc ∪ t.names) ∅
 
-def PropositionForest.wellFormed [DecidableEq Name] (f : PropositionForest Atom Name) : Prop :=
-  let trees := f.1.toList
-  (∀ t ∈ trees, Tree.wellFormed t) ∧
-  trees.Pairwise (fun t₁ t₂ => Disjoint t₁.names t₂.names)
+-- homogenous view on binary connectives
+inductive BinOp
+| tensor | parr | with | oplus
+deriving DecidableEq, BEq
+
+def viewBin {Atom : Type} :
+Proposition Atom → Option (BinOp × Proposition Atom × Proposition Atom)
+| Proposition.tensor A B  => some (BinOp.tensor, A, B)
+| Proposition.parr A B    => some (BinOp.parr, A, B)
+| Proposition.with A B    => some (BinOp.with, A, B)
+| Proposition.oplus A B   => some (BinOp.oplus, A, B)
+| _                       => none
+
+def prop_to_indexed_formulaTree {Atom : Type} (p : Proposition Atom) (counter : Nat) :
+Nat × FormulaTree Atom Nat :=
+  match p with
+  | .atom a => (counter + 1, ⟨Tree.leaf counter, fun _ => .atom a⟩)
+  | .atomDual a => (counter + 1, ⟨Tree.leaf counter, fun _ => .atomDual a⟩)
+  | .tensor A B
+  | .parr A B
+  | .oplus A B
+  | .with A B  =>
+    let (counter₁, FormTree₁) := prop_to_indexed_formulaTree A counter
+    let (counter₂, FormTree₂) := prop_to_indexed_formulaTree B counter₁
+    let op := match p with
+      | .tensor _ _ => .tensor
+      | .parr _ _ => .parr
+      | .oplus _ _ => .oplus
+      | .with _ _ => .with
+      | _ => .tensor -- this case is impossible, but we need to put something here
+    (counter₂, ⟨Tree.node counter [FormTree₁.1, FormTree₂.1], fun n =>
+      if h₁ : n.1 ∈ FormTree₁.1 then FormTree₁.2 ⟨n.1, h₁⟩
+      else if h₂ : n.1 ∈ FormTree₂.1 then FormTree₂.2 ⟨n.1, h₂⟩
+      else op⟩)
+  -- | .tensor A B =>
+  --   let (counter₁, FormTree₁) := prop_to_indexed_formulaTree A counter
+  --   let (counter₂, FormTree₂) := prop_to_indexed_formulaTree B counter₁
+  --   (counter₂, ⟨Tree.node counter [FormTree₁.1, FormTree₂.1], fun n =>
+  --     if h₁ : n.1 ∈ FormTree₁.1 then FormTree₁.2 ⟨n.1, h₁⟩
+  --     else if h₂ : n.1 ∈ FormTree₂.1 then FormTree₂.2 ⟨n.1, h₂⟩
+  --     else .tensor⟩)
+  -- | .parr A B =>
+  --   let (counter₁, FormTree₁) := prop_to_indexed_formulaTree A counter
+  --   let (counter₂, FormTree₂) := prop_to_indexed_formulaTree B counter₁
+  --   (counter₂, ⟨Tree.node counter [FormTree₁.1, FormTree₂.1], fun n =>
+  --     if h₁ : n.1 ∈ FormTree₁.1 then FormTree₁.2 ⟨n.1, h₁⟩
+  --     else if h₂ : n.1 ∈ FormTree₂.1 then FormTree₂.2 ⟨n.1, h₂⟩
+  --     else .parr⟩)
+  -- | .oplus A B =>
+  --   let (counter₁, FormTree₁) := prop_to_indexed_formulaTree A counter
+  --   let (counter₂, FormTree₂) := prop_to_indexed_formulaTree B counter₁
+  --   (counter₂, ⟨Tree.node counter [FormTree₁.1, FormTree₂.1], fun n =>
+  --     if h₁ : n.1 ∈ FormTree₁.1 then FormTree₁.2 ⟨n.1, h₁⟩
+  --     else if h₂ : n.1 ∈ FormTree₂.1 then FormTree₂.2 ⟨n.1, h₂⟩
+  --     else .oplus⟩)
+  -- | .with A B  =>
+  --   let (counter₁, FormTree₁) := prop_to_indexed_formulaTree A counter
+  --   let (counter₂, FormTree₂) := prop_to_indexed_formulaTree B counter₁
+  --   (counter₂, ⟨Tree.node counter [FormTree₁.1, FormTree₂.1], fun n =>
+  --     if h₁ : n.1 ∈ FormTree₁.1 then FormTree₁.2 ⟨n.1, h₁⟩
+  --     else if h₂ : n.1 ∈ FormTree₂.1 then FormTree₂.2 ⟨n.1, h₂⟩
+  --     else .with⟩)
+
+def prop_to_formulaTree {Atom : Type} (p : Proposition Atom) : FormulaTree Atom Nat :=
+  (prop_to_indexed_formulaTree p 0).2
+
+
+#check prop_to_formulaTree (Proposition.oplus (Proposition.atom "a") (Proposition.tensor (Proposition.atom "b") (Proposition.atomDual "c")))
+
+theorem propToFormulaTree_is_wellFormed {Atom : Type}
+  (p : Proposition Atom) : (prop_to_formulaTree p).wellFormed := by
+  induction p
+  case atom a =>
+    simp [prop_to_formulaTree, prop_to_indexed_formulaTree]
+    constructor
+    · simp [Tree.wellFormed]
+    · intros n hn
 
 
 ----------------------------
--- Formula Tree of a Formula
+-- Formula-forests (sequents)
 ----------------------------
 
-def formulaTreeOf (F Proposition Atom) [DecidableEq Atom] : PropositionTree Atom Name :=
-
--- it should keep a buffer to ensure that names are fresh every time we call it recursively,
-  sorry
-
-
--- theorem : formula tree is wellFormed
-
--- formula forest of sequent
-
--- theorem : formula forest is wellFormed
+-- A forest of formulas labeled by Nodes
+def FormulaForest (Atom Nat : Type u) [DecidableEq Nat] :=
+  Σ (f : Forest Nat), (t : { t : Tree Nat // t ∈ₜ f }) →
+    (n : { n : Nat // n ∈ t }) → FormulaTree.Value Atom
 
 
+-- Check that a formula forest is well-formed
+def FormulaForest.wellFormed {Atom Nat : Type u} [DecidableEq Nat]
+  (f : FormulaForest Atom Nat) : Prop :=
+  let ⟨forest, labeling⟩ := f
+  Forest.wellFormed forest
+  ∧
+  ∀ (t : { t : Tree Nat // t ∈ₜ forest }) (n : { n : Nat // n ∈ t }),
+    match labeling t n with
+    | .atom _     => n.1 ∈ t.1.leaves
+    | .atomDual _ => n.1 ∈ t.1.leaves
+    | .tensor     => n.1 ∉ t.1.leaves
+    | .parr       => n.1 ∉ t.1.leaves
+    | .oplus      => n.1 ∉ t.1.leaves
+    | .with       => n.1 ∉ t.1.leaves
 
-----------------------------
--- Cotrees (for proofnets)
-----------------------------
+
+-- check if a formula tree is in the formula forest
+def FormulaTree_in_FormulaForest {Atom Nat : Type u} [DecidableEq Nat]
+  (t : FormulaTree Atom Nat)
+  (f : FormulaForest Atom Nat) : Prop :=
+  ∃ (ht : t.1 ∈ₜ f.1),
+    ∀ (n : Nat) (hn : n ∈ t.1),
+      t.2 ⟨n, hn⟩ = f.2 ⟨t.1, ht⟩ ⟨n, hn⟩
+
+-- check if a forest is a subforest of another forest
+def subForest {Atom Nat : Type u} [DecidableEq Nat] (f₁ f₂ : FormulaForest Atom Nat) : Prop :=
+  ∀ t₁ : FormulaTree Atom Nat,
+    FormulaTree_in_FormulaForest t₁ f₁ → FormulaTree_in_FormulaForest  t₁ f₂
 
 
--- CoTree of type a sequent is a tree with leaves subtrees of the formula tree of the sequent, and internal nodes conc or conf
 
--- CoTree is canonical if no conc or conf node ha either 1 child or children with root the same label
+-- ----------------------------
+-- -- Cotrees (for proofnets)
+-- ----------------------------
 
--- a proof structure is a canonical CoTree with leaves atomomic
+
+-- -- CoTree of type a sequent is a tree with leaves subtrees
+-- -- of the formula tree of the sequent, and internal nodes conc or conf
+
+-- -- CoTree is canonical if no conc or conf node has either 1 child
+-- -- or children with root the same label
+
+-- -- a proof structure is a canonical CoTree with leaves atomomic
 
 
 
